ba99e92d23888c362b1e4a5325ec17791d08bcc9
Kam Radhakrishnan <karadhak@adobe.com>
2016-07-25 16:40:46 +1000

    Doco

--- ComponentUsageReport/componentUsageReport.sh
+++ ComponentUsageReport/componentUsageReport.sh
@@ -26,33 +26,39 @@ printf "\nProcessing........\n"
 rm -rf ./*.csv
 rm -rf ./*.out
 
-eval curl -G  --silent  -u admin:admin --data-urlencode \""stmt=/jcr:root${APP_PATH}//*[@jcr:primaryType = 'cq:Component']"\" --data-urlencode '_charset_=utf-8' --data-urlencode 'type=xpath' --data-urlencode 'showResults=true' 'http://localhost:4502/crx/de/query.jsp' > query.out
+eval curl -G  --silent  -u ${upass} --data-urlencode \""stmt=/jcr:root${APP_PATH}//*[@jcr:primaryType = 'cq:Component']"\" --data-urlencode '_charset_=utf-8' --data-urlencode 'type=xpath' --data-urlencode 'showResults=true' 'http://${host}:${port}/crx/de/query.jsp' > query.out
 
 cat query.out  | grep -o '"path":"[a-z._A-Z0-9/:-]*"' | grep -o '/[^"]*' | sort > sort.out
 
-echo "Component Title, No. Of Occurances across the site, Component Path, Activation status" >> ./draft.csv
+echo "Component Title, Component Path, No. Of Occurances across the site, Component Path, Activation status" >> ./draft.csv
 
 while read FILE_PATH; do
 
        R_TYPE=$(echo "${FILE_PATH}" | sed 's|/apps/||')
-       
-	  C_TITLE=$(curl -sL -u ${upass} http://${host}:${port}/${FILE_PATH}.json | grep -o '"jcr:title":"[A-Za-z0-9 ]*"')
+       	  
+	  C_TITLE=$(curl -sL -u ${upass} http://${host}:${port}/${FILE_PATH}.json | grep -o '"jcr:title":"[A-Za-z0-9()_&,.? -]*"')
 	         
 	  eval curl -G  -silent -u ${upass} --data-urlencode \""stmt=/jcr:root${CONTENT_PATH}//*[@sling:resourceType = '${R_TYPE}']"\" --data-urlencode '_charset_=utf-8' --data-urlencode 'type=xpath' --data-urlencode 'showResults=true' 'http://${host}:${port}/crx/de/query.jsp' > query1.out
 	  
 	  cat query1.out  | grep -o '"path":"[a-z._A-Z0-9/:-]*"' | grep -o '/[^"]*' > pages.out
 	  
-	  COUNT=$(cat query1.out | grep -o '"total":[0-9]*')
+	  COUNT=$(wc -l pages.out)
 	  
 	  while read PAGE_PATH; do
 	  
-	  	ONLY_PAGE_PATH=$(echo ${PAGE_PATH} | grep -o "/content/(.*)jcr:content")
-	  
-	  	PUB_STATUS=$(curl -sL -u ${upass} http://${host}:${port}/${ONLY_PAGE_PATH}.json | grep -o '"cq:lastReplicationAction":"[A-Za-z0-9 ]*"')
+	  	ONLY_PAGE_PATH=$(echo ${PAGE_PATH} | grep -o "/content/[a-zA-Z0-9 /&.-]*jcr:content")
+		
+		echo $ONLY_PAGE_PATH
+	  	
+	  	PUB_STATUS=$(curl -sL -u ${upass} http://${host}:${port}${ONLY_PAGE_PATH}.json | grep -o '"cq:lastReplicationAction":"[A-Za-z0-9 -]*"')
+		
+		echo $PUB_STATUS
 
-		PUB_STATUS=$([ ! -z "$PUB_STATUS" ] || echo "Not-activated")
+		if ( "$PUB_STATUS" == "" ) then  
+		    PUB_STATUS="Not-Activated"
+		fi
 	  
-	  	echo "${C_TITLE}, ${COUNT}, ${PAGE_PATH}, ${PUB_STATUS}" >> ./draft.csv
+	  	echo "${C_TITLE}, ${FILE_PATH}, ${COUNT}, ${PAGE_PATH}, ${PUB_STATUS}" >> ./draft.csv
 	  
 	  done <pages.out
 
@@ -61,5 +67,5 @@ done <sort.out
 cat draft.csv | sed 's|\"jcr:title\":||' |  sed 's|\"total\":||' > final.csv
 
 printf "\nProcessing Complete. Output in file:: final.csv\n"
-open ./final.csv
+#open ./final.csv
 
